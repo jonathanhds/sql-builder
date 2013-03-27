ce847b47cfe8bfb3d4ee16a1e795058b0e3e4200
Jonathan Henrique de Souza <jonathanhds@gmail.com>
2013-03-26 21:55:59 -0300

    Add UPDATE statement builder

---
+++ src/test/java/com/github/sqlbuilder/UpdateQueryTest.java
@@ -0,0 +1,46 @@
+package com.github.sqlbuilder;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UpdateQueryTest {
+
+	@Test(expected = IllegalQueryException.class)
+	public void shouldThrowErroWhenSetIsNotPassed() throws Exception {
+		// Given
+		UpdateQuery query = new UpdateQuery("persons p");
+
+		// When
+		query.toString();
+	}
+
+	@Test
+	public void shouldCreateQueryForTableUsingSets() throws Exception {
+		// Given
+		UpdateQuery query = new UpdateQuery("persons p").set("p.name", "foo");
+
+		// When
+		String actual = query.toString();
+
+		// Then
+		String expected = "UPDATE persons p SET p.name = 'foo'";
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void shouldCreateQueryUsingWhere() throws Exception {
+		// Given
+		UpdateQuery query = new UpdateQuery("employee e").set("e.salary", "50000")
+														 .addWhere("e.age > 40")
+														 .addWhere("e.genre = 'female'");
+
+		// When
+		String actual = query.toString();
+
+		// Then
+		String expected = "UPDATE employee e SET e.salary = '50000' WHERE e.age > 40 AND e.genre = 'female'";
+		assertEquals(expected, actual);
+	}
+
+}
